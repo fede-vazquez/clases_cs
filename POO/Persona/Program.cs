0e881a0557267105bce7f8c332c62b688a71f25c
Fede <fedenv2000@gmail.com>
2024-08-01 15:00:33 -0300

    Agrega ejemplo de clase Persona, para explicar getters y setters.

---
+++ POO/Persona/Program.cs
@@ -0,0 +1,36 @@
+﻿class Persona
+{
+    private string _nombre = "Nombre default";
+    private int _edad;
+
+    public string Nombre
+    {
+        get { return _nombre; }
+        set { _nombre = value; }
+    }
+
+    public int Edad
+    {
+        get { return _edad; }
+        set { _edad = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Forma de utilizar directamente los setters. usar llaves en lugar de parentesis, y dentro poner los atributos de la clase y sus datos.
+        Persona persona = new Persona
+        {
+            Nombre = "Jorguito",
+            Edad = 23
+        };
+        
+        //persona.Nombre = "Jorguito";
+        //persona.Edad = 23;
+
+        Console.WriteLine($"Nombre: {persona.Nombre}");
+        Console.WriteLine($"Edad: {persona.Edad}");
+    }
+}
