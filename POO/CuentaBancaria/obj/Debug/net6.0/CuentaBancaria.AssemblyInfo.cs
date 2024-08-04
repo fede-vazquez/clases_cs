a877ef205161d7835c21e96ee43887159c6bfcd1
FedeVazquez <fedenv2000@gmail.com>
2024-08-03 21:50:00 -0300

    Cambia los nombres de las carpetas de POO al nombre de su solución. Y crea .gitignore para la carpeta de .vs

--- POO/CuentaBancaria/obj/Debug/net6.0/CuentaBancaria.AssemblyInfo.cs
+++ POO/CuentaBancaria/obj/Debug/net6.0/CuentaBancaria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CuentaBancaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b4cebbf35685003399b26ff3367b3b29cf22463")]
 [assembly: System.Reflection.AssemblyProductAttribute("CuentaBancaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CuentaBancaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
