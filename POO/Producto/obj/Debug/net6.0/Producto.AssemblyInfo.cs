9552ec026b094896093c610e33376f8c41bf7a83
Fede <fedenv2000@gmail.com>
2024-08-07 14:34:34 -0300

    Arregla error con los nombres de las carpetas de algunasa soluciones.

--- POO/Producto/obj/Debug/net6.0/Producto.AssemblyInfo.cs
+++ POO/Producto/obj/Debug/net6.0/Producto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Producto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b4cebbf35685003399b26ff3367b3b29cf22463")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Producto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Producto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
