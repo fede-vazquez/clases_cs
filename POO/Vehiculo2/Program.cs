d67ff4e5ef7c7229bb6f1496b3c0607b848ca694
Fede <fedenv2000@gmail.com>
2024-08-07 14:38:52 -0300

    Se crea clase Vehiculo2 para explicar sobre los métodos protegidos y su alcance con las clases hijas.

---
+++ POO/Vehiculo2/Program.cs
@@ -0,0 +1,41 @@
+﻿class Vehiculo
+{
+    public string Marca { get; set; }
+    public string Modelo { get; set; }
+
+    public Vehiculo(string marca, string modelo)
+    {
+        Marca = marca;
+        Modelo = modelo;
+    }
+
+    protected void MostrarDetalle()
+    {
+        Console.WriteLine($"Marca: {Marca}, Modelo: {Modelo}");
+    }
+}
+
+class Coche : Vehiculo
+{
+    public int NumeroPuertas { get; set; }
+
+    public Coche(string marca, string modelo, int numeroPuertas) : base(marca, modelo)
+    {
+        NumeroPuertas = numeroPuertas;
+    }
+
+    public void MostrarInformacionCoche()
+    {
+        MostrarDetalle(); // Llamada al método protegido de la clase base.
+        Console.WriteLine($"Número de puertas: {NumeroPuertas}.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Coche coche = new Coche("Toyota", "Corolla", 4);
+        coche.MostrarInformacionCoche();
+    }
+}
