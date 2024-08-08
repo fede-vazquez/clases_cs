0b651ad99b15e38e6577e8996a6f3806180458dd
FedeVazquez <fedenv2000@gmail.com>
2024-08-08 11:11:52 -0300

    Agrega carpeta con ejemplos de inputs y outputs en POO-IO.

---
+++ POO-IO/Calculos/Program.cs
@@ -0,0 +1,26 @@
+﻿int n1, n2;
+
+Console.Write("Ingrese un número: ");
+int.TryParse(Console.ReadLine(), out n1);
+
+Console.Write("Ingrese otro núero: ");
+int.TryParse(Console.ReadLine(), out n2);
+
+int result = n1 + n2;
+Console.WriteLine($"La suma de los números es: {result}");
+
+result = n1 - n2;
+Console.WriteLine($"La resta de los números es: {result}");
+
+result = n1 * n2;
+Console.WriteLine($"La multiplicación de los números es: {result}");
+
+if (n2 == 0)
+{
+    Console.WriteLine($"Él segundo número es cero, no se puede dividir.");
+}
+else
+{
+    float r = (float)n1 / (float)n2;
+    Console.WriteLine($"La división de los números es: {r}");
+}
---
+++ POO-IO/Vehiculo/Program.cs
@@ -0,0 +1,74 @@
+﻿class Vehiculo
+{
+    public string Modelo { get; set; }
+    public string Marca { get; set; }
+
+    public Vehiculo(string modelo, string marca)
+    {
+        Modelo = modelo;
+        Marca = marca;
+    }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Marca: {Marca}, Modelo: {Modelo}");
+    }
+}
+
+class VehiculoCuatroRuedas : Vehiculo
+{
+    public int NumeroPuertas { get; set; }
+
+    public VehiculoCuatroRuedas(string modelo, string marca, int numeroPuertas) : base(modelo, marca)
+    {
+        NumeroPuertas = numeroPuertas;
+    }
+
+    public void MostrarInformacionCuatroRuedas()
+    {
+        MostrarInformacion();
+        Console.WriteLine($"NumeroPuertas: {NumeroPuertas}");
+    }
+}
+
+class Coche : VehiculoCuatroRuedas
+{
+    public string TipoCombustible { get; set; }
+
+    public Coche(string modelo, string marca, int numeroPuertas, string tipoCombustible) : base(modelo, marca, numeroPuertas)
+    {
+        TipoCombustible = tipoCombustible;
+    }
+
+    public void MostrarInformacionCoche()
+    {
+        MostrarInformacionCuatroRuedas();
+        Console.WriteLine($"Tipo de combustible: {TipoCombustible}");
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        string marca, modelo, tipoCombustible;
+        int numeroPuertas;
+
+        Console.Write("Ingrese marca del coche: ");
+        marca = Console.ReadLine();
+        
+        Console.Write("Ingrese modelo del coche: ");
+        modelo = Console.ReadLine();
+
+        Console.Write("Ingrese el numero de puertas del coche: ");
+        int.TryParse(Console.ReadLine(), out numeroPuertas);
+
+        Console.Write("Ingrese tipo de combustible del coche: ");
+        tipoCombustible = Console.ReadLine();
+
+        Coche coche = new Coche(modelo, marca, numeroPuertas, tipoCombustible);
+
+        coche.MostrarInformacionCoche();
+    }
+}
