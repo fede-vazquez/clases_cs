07430e5c02101f14fdfb710c47c8eed5ee91a22f
FedeVazquez <fedenv2000@gmail.com>
2024-09-06 00:08:20 -0300

    Agrega clase Asiento en la solución Cine.

---
+++ Cine/Cine/Modelos/Asiento.cs
@@ -0,0 +1,21 @@
+﻿using Cine.Enums;
+
+namespace Cine.Modelos
+{
+    public class Asiento
+    {
+        public char Letra { get; private set; }
+        public int Numero { get; private set; }
+        public TipoAsiento Tipo { get; private set; }
+        public bool Ocupado { get; private set; } = false;
+
+        public Asiento(char letra, int numero,  TipoAsiento tipo)
+        {
+            Letra = letra;
+            Numero = numero;
+            Tipo = tipo;
+        }
+
+        public void CambiarOcupado() { Ocupado = !Ocupado; }
+    }
+}
--- Cine/Cine/obj/Debug/net6.0/Cine.AssemblyInfo.cs
+++ Cine/Cine/obj/Debug/net6.0/Cine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+520962bb767fb59994dfdf6358775e5f564cced7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c01d5ff520514d0b7ce8d24761793622133a7bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
