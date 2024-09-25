6d5e1296f46b430c6c8d90d2ee57557ae060642b
fede <fedenv2000@gmail.com>
2024-09-25 17:44:15 -0300

    Agrega solución de LineaProducto.

---
+++ Colecciones/LineaProducto/Program.cs
@@ -0,0 +1,28 @@
+﻿class Producto
+{
+    public string Nombre { get; set; }
+    public int TiempoProcesamiento { get; set; }
+
+    public Producto(string nombre, int tiempoProcesamiento)
+    {
+        Nombre = nombre;
+        TiempoProcesamiento = tiempoProcesamiento;
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Queue<Producto> lineaProducto = new Queue<Producto>();
+
+        lineaProducto.Enqueue(new Producto("Laptop", 6));
+        lineaProducto.Enqueue(new Producto("Telefono", 8));
+        lineaProducto.Enqueue(new Producto("Mouse", 2));
+        
+        while(lineaProducto.Count > 0)
+        {
+            Producto unProducto = lineaProducto.Dequeue();
+            Console.WriteLine($"Ensamblando {unProducto.Nombre} por {unProducto.TiempoProcesamiento}");
+        }
+    }
+}
